86cfd14e13c1c1a45a53caa18fb673f5756b079d
bagobob <htibago@gmail.com>
2023-03-24 01:17:24 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,11 @@ import {
 import { useTranslation } from 'react-i18next';
 import bagImg from "../assets/bg-illustration-removebg.png";
 import {Link} from 'react-scroll';
+import Scroll from './Scroll';
 
 export const Hero = () => {
 
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
   return (
     <div name="home" className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
         <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
@@ -25,9 +26,9 @@ export const Hero = () => {
             <div >
                 <img className="w-full object-cover" src={bagImg} alt="" />
             </div>
-            <div className='absolute flex flex-col py-8 md:min-w-[760px]
+            <div className={`absolute flex flex-col md:min-w-[760px]
             bottom-[5%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
-            border border-slate-300 rounded-xl text-center shadow-xl'>
+            border border-slate-300 rounded-xl text-center shadow-xl ${i18n.language === 'fr' ? 'py-4' : 'py-8'}`}>
                 <p>{t('our_services')}</p>
                 <div className='flex justify-between flex-wrap px-4'>
                     <p className='flex px-4 py-2 text-slate-500 cursor-pointer link'><ServerIcon className='h-8 text-indigo-600   px-4 py-2 ' />
@@ -45,6 +46,7 @@ export const Hero = () => {
                 </div>
             </div>
         </div>
+        <Scroll />
     </div>
 )
 }
